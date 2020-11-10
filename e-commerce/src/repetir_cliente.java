ef311e5ef16a097649ec5c4afea2c36b3b20e497
RicardoMagalhaesRFinkelstein <ricardomrfin@gmail.com>
2020-11-09 21:29:54 -0300

    SUBINDO ESTRUTURA PARA REPETIR COMPRA

---
+++ e-commerce/src/repetir_cliente.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class repetir_cliente {
+
+	public static void main(String[] args) {
+	java.util.Scanner leia = new Scanner(System.in);
+	char proximoCliente;
+	
+	//System.out.println("Próximo cliente S ou N");
+	//proximoCliente = leia.next().toUpperCase().charAt(0);
+	
+	do
+	{
+		
+		System.out.println("\n Maçã");
+		System.out.println("Próximo cliente S ou N");
+		proximoCliente = leia.next().toUpperCase().charAt(0);
+		//Repetir todo o processo de compra, desde a seleção até o pagamento
+	}
+	while (proximoCliente == 'S'); 
+	
+		System.out.println("Obrigado pelas compras, programa encerrado");
+	
+
+	}
+
+}
