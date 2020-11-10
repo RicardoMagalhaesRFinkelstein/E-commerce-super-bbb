9751b043c8894daa4bf6edabd72a699f4e117de1
RicardoMagalhaesRFinkelstein <ricardomrfin@gmail.com>
2020-11-10 13:10:31 -0300

    Subindo vetores de produtos

---
+++ e-commerce/src/definicao_produtos.java
@@ -0,0 +1,11 @@
+
+public class definicao_produtos {
+	public static void main(String[] args) {
+		
+		String produtos[] = {"leite","biscoito","Veja","revista","cloro","refrigerante","parafuso","lâmpada","papel-toalha","papel higiênico"};
+		String unidadeProdutos[] = {"litro","pacote","unidade","unidade","litro","unidade","pacote","unidade","pacote","pacote"};
+		
+		
+	}
+
+}
